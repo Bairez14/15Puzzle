e09b1ca456107910c1c046db4b4a5143387f9edb
Bairez14 <Bairez14@users.noreply.github.com>
2021-11-30 17:38:38 -0600

    switched things around

--- 15Puzzle/Fall2021Project4_15Puzzle/src/main/java/JavaFXTemplate.java
+++ 15Puzzle/Fall2021Project4_15Puzzle/src/main/java/JavaFXTemplate.java
@@ -21,12 +21,9 @@ public class JavaFXTemplate extends Application {
 	Button start;
 	public GridPane gp;
 	public Text moveNum;
-	public HashMap<String, Scene> sceneMap;
-	public BorderPane bPane;
-	public ObservableList<String> stats;
-	public Button[][] buttons = new Button[4][4];
-
-	
+	//public HashMap<String, Scene> sceneMap;
+	//public ObservableList<String> stats;
+	//public Button[][] buttons = new Button[4][4];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -36,70 +33,29 @@ public class JavaFXTemplate extends Application {
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		//sceneMap.put("welcomeScene", welcomeScene(primaryStage));
-		sceneMap.put("puzzleScene", gamePlayScene(primaryStage));
-		primaryStage.setTitle("Welcome to 15 Puzzle");
-		//Scene scene = new Scene(new VBox(), 700,700);
-		primaryStage.setScene(sceneMap.get("puzzleScene"));
-		//primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		Thread t = new Thread(()-> {A_IDS_A_15solver ids = new A_IDS_A_15solver();});
+		Text welcome = new Text();
+		start = new Button("Start Game");
+		welcome.setText("Welcome to 15 Puzzle!");
+		welcome.setX(320);
+		welcome.setY(350);
+		HBox welcomeBox = new HBox(welcome, start);
+		Scene welcomeScene = new Scene(welcomeBox);
+		welcomeScene.getRoot().setStyle("-fx-font-family: 'serif';"
+				+ "-fx-background-image:url(https://cdn.shopify.com/s/files/1/0572/8311/7249/files/1991_Start_1024x1024.png?v=1626712496)");
+		start.setOnAction(primaryStage.setScene(welcomeScene()));
+
+		Thread t = new Thread(() -> {
+			A_IDS_A_15solver ids = new A_IDS_A_15solver();
+		});
 		t.start();
 
 	}
-	
-	// public Scene welcomeScene(Stage primaryStage) {
-	// 	Text welcome = new Text();
-	// 	welcome.setText("Welcome to 15 Puzzle!");
-	// 	welcome.setX(320);
-	// 	welcome.setY(350);
 
-	// 	//Scene scene = new Scene(, 700, 700);
-
-	// 	return null;
-	// }
-
-	public Scene gamePlayScene(Stage primaryStage) {
-		gp = new GridPane();
-		// setting up menubar
-		MenuBar mBar = new MenuBar();
-		Menu hMenu = new Menu("Heuristics");
-		AIH1 = new MenuItem("Heuristic 1");
-		AIH2 = new MenuItem("Heurostic 2");
-		hMenu.getItems().addAll(AIH1,AIH2);
-		Menu settingMenu = new Menu("Settings");
-		exit = new MenuItem("Exit");
-		newGame = new MenuItem("Reset");
-		settingMenu.getItems().addAll(exit,newGame);
-		Menu solutionMenu = new Menu("Help");
-		seeSolution = new MenuItem("Solution");
-		solutionMenu.getItems().add(seeSolution);
-		mBar.getMenus().addAll(hMenu,settingMenu, solutionMenu);
-		start = new Button("Start Game");
-		//creating grid
-		gp.setAlignment(Pos.CENTER);
-		gp.setHgap(10);
-		gp.setVgap(10);
-		addGrid(gp);
-		HBox gameBox = new HBox(mBar, start,gp);
-		Scene puzzleScene = new Scene(gameBox);
-		puzzleScene.getRoot().setStyle("-fx-font-family: 'serif';"
-				+ "-fx-background-image: url(https://cdn.vox-cdn.com/thumbor/rh4HfDnP3aSyhDNC6wrRhqiYZzc=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19246117/tyler_parker_5_guys_bulls_alyceatinoyan.jpg);");
-
-		return puzzleScene;
-	}
-
-	// public Scene winScene(Stage primaryStage) {
-	// 	return null;
-	// }
-	
 	public void addGrid(GridPane grid) {
 		for (int x = 0; x < 4; x++) { // column
 			for (int i = 0; i < 4; i++) { // row
 				Button b1 = new Button();
-				//buttons[x][i] = b1; // populates buttons to 2d array of buttons
+				// buttons[x][i] = b1; // populates buttons to 2d array of buttons
 				b1.setPrefSize(90, 90);
 				b1.setStyle("-fx-background-color: transparent;" + "-fx-border-color: white;");
 				grid.add(b1, x, i); // adding buttons to grid
@@ -107,4 +63,81 @@ public class JavaFXTemplate extends Application {
 		}
 	}
 
+	public Scene puzzleScene() {
+		gp = new GridPane();
+		// setting up menubar
+		MenuBar mBar = new MenuBar();
+		Menu hMenu = new Menu("Heuristics");
+		AIH1 = new MenuItem("Heuristic 1");
+		AIH2 = new MenuItem("Heurostic 2");
+		hMenu.getItems().addAll(AIH1, AIH2);
+		Menu settingMenu = new Menu("Settings");
+		exit = new MenuItem("Exit");
+		newGame = new MenuItem("Reset");
+		settingMenu.getItems().addAll(exit, newGame);
+		Menu solutionMenu = new Menu("Help");
+		seeSolution = new MenuItem("Solution");
+		solutionMenu.getItems().add(seeSolution);
+		mBar.getMenus().addAll(hMenu, settingMenu, solutionMenu);
+		// creating grid
+		gp.setAlignment(Pos.CENTER);
+		gp.setHgap(10);
+		gp.setVgap(10);
+		addGrid(gp);
+		HBox gameBox = new HBox(mBar, gp);
+		Scene puzzleScene = new Scene(gameBox, 700, 700);
+		puzzleScene.getRoot().setStyle("-fx-font-family: 'serif';"
+				+ "-fx-background-image:url(https://www.nba.com/bulls/sites/bulls/files/1920-generic-bullhead_0.png)");
+
+		primaryStage.setScene(puzzleScene);
+		primaryStage.show();
+
+		// event handlers
+		exit.setOnAction(e -> System.exit(0));
+	}
+
+	// public void resetGame(GridPane grid) {
+	// 	for (Node n : grid.getChildren()) {
+	// 		Button temp = (Button) n;
+	// 		temp.setText("");
+	// 		temp.setDisable(false);
+	// 		temp.setStyle("-fx-background-color: transparent;" + "-fx-border-color: white;");
+	// 	}
+	// }
 }
+
+	// public Scene gamePlayScene() {
+	// 	//gp = new GridPane();
+	// 	// setting up menubar
+	// 	// MenuBar mBar = new MenuBar();
+	// 	// Menu hMenu = new Menu("Heuristics");
+	// 	// AIH1 = new MenuItem("Heuristic 1");
+	// 	// AIH2 = new MenuItem("Heurostic 2");
+	// 	// hMenu.getItems().addAll(AIH1,AIH2);
+	// 	// Menu settingMenu = new Menu("Settings");
+	// 	// exit = new MenuItem("Exit");
+	// 	// newGame = new MenuItem("Reset");
+	// 	// settingMenu.getItems().addAll(exit,newGame);
+	// 	// Menu solutionMenu = new Menu("Help");
+	// 	// seeSolution = new MenuItem("Solution");
+	// 	// solutionMenu.getItems().add(seeSolution);
+	// 	// mBar.getMenus().addAll(hMenu,settingMenu, solutionMenu);
+	// 	start = new Button("Start Game");
+	// 	//creating grid
+	// 	// gp.setAlignment(Pos.CENTER);
+	// 	// gp.setHgap(10);
+	// 	// gp.setVgap(10);
+	// 	// addGrid(gp);
+	// 	HBox gameBox = new HBox(start);
+	// 	Scene puzzleScene = new Scene(gameBox,700, 700);
+	// 	// puzzleScene.getRoot().setStyle("-fx-font-family: 'serif';"
+	// 	// 		+ "-fx-background-image: url(https://cdn.vox-cdn.com/thumbor/rh4HfDnP3aSyhDNC6wrRhqiYZzc=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19246117/tyler_parker_5_guys_bulls_alyceatinoyan.jpg);");
+	// 	// sceneMap.put("puzzleScene", puzzleScene);
+	// 	return puzzleScene;
+	// }
+
+	// public Scene winScene(Stage primaryStage) {
+	// 	return null;
+	// }
+
+
