9dc278c4e608df58ea72a17b1eb3b711d7eaea1e
Bairez14 <Bairez14@users.noreply.github.com>
2021-11-30 00:03:21 -0600

    UI
    Co-authored-by: xlzth <xlzth@users.noreply.github.com>

--- 15Puzzle/Fall2021Project4_15Puzzle/src/main/java/JavaFXTemplate.java
+++ 15Puzzle/Fall2021Project4_15Puzzle/src/main/java/JavaFXTemplate.java
@@ -1,11 +1,23 @@
+import java.util.HashMap;
+import java.util.*;
+
 import javafx.application.Application;
 
 import javafx.scene.Scene;
-
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 public class JavaFXTemplate extends Application {
+
+	public Button AIH1, AIH2, start, exit, seeSolution, newGame;
+	public GridPane gp;
+	public Text moveNum;
+	public HashMap<String, Scene> sceneMap;
+
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -28,6 +40,26 @@ public class JavaFXTemplate extends Application {
 		Thread t = new Thread(()-> {A_IDS_A_15solver ids = new A_IDS_A_15solver();});
 		t.start();
 
+	}
+	
+	public Scene welcomeScreen(Stage primaryStage){
+		Text welcome = new Text();
+		welcome.setText("Welcome to 15 Puzzle!");
+		welcome.setX(320);
+		welcome.setY(350);
+
+		//Scene scene = new Scene(, 700, 700);
+
+		return null;
+	}
+
+	public Scene gamePlayScene(Stage primaryStage){
+
+		return null;
+	}
+
+	public Scene winScene(Stage primaryStage){
+		return null;
 	}
 
 }
